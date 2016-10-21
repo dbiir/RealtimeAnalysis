238d5a662db0c1a3691580d8b31954aa39f20d27
Jin Guodong <jelly.guodong.jin@gmail.com>
2016-10-21 20:28:06 +0800

    change running sh

--- dist/sbin/loader-daemon.sh
+++ dist/sbin/loader-daemon.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-    echo $0: usage: topic partitionBegin partitionEnd
-    exit 1
-fi
-
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ".." && pwd )"
 
-topic=$1
-parB=$2
-parE=$3
-
-java -jar $BASEDIR/bin/Loader.jar $BASEDIR/conf/config.props $topic $parB $parE &
-echo "Loader started... Loading from topic [$topic], partition id from [$parB] to [$parE]."
+java -jar $BASEDIR/bin/Loader.jar $BASEDIR/conf/config.props &
+echo "Loader started...."
--- dist/sbin/start-loaders.sh
+++ dist/sbin/start-loaders.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-    echo $0: usage: topic partitionBegin partitionEnd
-    exit 1
-fi
-
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ".." && pwd )"
 SBINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-topic=$1
-parB=$2
-parE=$3
 
 while read -r host
 do
     echo "Logging into $host, and start Loader"
-    ssh $USER@$host -n "cd '$SBINDIR' && nohup ./loader-daemon.sh $topic $parB $parE > $BASEDIR/running 2>&1 &"
+    ssh $USER@$host -n "cd '$SBINDIR' && nohup ./loader-daemon.sh > $BASEDIR/running 2>&1 &"
 done < $BASEDIR/loaders
